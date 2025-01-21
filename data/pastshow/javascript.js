c9d137deb8a6b110bc58a77cc0c3fc8d37d7cb3f
tora <aidatorajiro@users.noreply.github.com>
2025-01-22 02:42:46 +0900

    Update javascript.js: jekyll issue???

--- data/pastshow/javascript.js
+++ data/pastshow/javascript.js
@@ -1,11 +1,4 @@
-(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-ga('create', 'UA-53823792-2', 'auto');
-ga('send', 'pageview');
-
-locate=location.href.match(/([^/]+)\.html/)[1];
+locate=location.href.match(/([^/]+)(\.html)?$/);
 window.onload=function(){
 	html='<table>';
 	html+='<tr>';
